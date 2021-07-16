6d7a1b53a48e69d648a9852a60763d9d110e2de8
Kevin R. Coombes <krc@silicovore.com>
2021-07-16 14:20:02 +0000

    version 1.3.1

---
+++ R/07-memberPlot.R
@@ -0,0 +1,36 @@
+memberPlot <- function(bindat, pal = NULL,
+                       xlab = "Members", ylab = "Categories") {
+  ## Ensure that it is really a binary matrix
+  if(any(is.na(bindat))) stop("Data contains mssing values.")
+  if(!all(bindat %in% 0:1)) stop("Matrix values must equal '0' or '1'.")
+  if(is.null(pal)) {
+    pal <- c("gray85",
+             light.colors(24)[c(4, 3, 23, 9, 24,
+                                7, 18, 11, 14, 5,
+                                2, 15, 16, 21)],
+             "purple3")
+  }
+  while(length(pal) < nrow(bindat)) pal <- c(pal, pal[-1])
+  # rows are categories, columns are samples
+
+  counts <- apply(bindat, 1, sum)  # number of members of each category
+  M <- 1:nrow(bindat)
+  bindat <- sweep(bindat, 1, M, "*")
+  bindat <- bindat[rev(order(counts)),] # sort rows by membership
+  
+  # order columns successively by row membership
+  bd <- as.list(as.data.frame(t(bindat))) # temporary, for ordering
+  oo <- do.call(order, bd)
+  bindat <- bindat[, rev(oo)]
+  ocount <- apply(bindat, 2, sum)
+  bindat <- bindat[, ocount > 0]
+  image(1:ncol(bindat), 1:nrow(bindat), t(bindat),
+        col = pal[1:(1+nrow(bindat))],
+        xlab = xlab, ylab = ylab, yaxt = "n")
+  mtext(rownames(bindat), side =2, las = 2, at = 1:nrow(bindat), line = 1)
+  text(ncol(bindat)/2, 1:nrow(bindat),
+       paste("N =", apply(bindat > 0, 1, sum)),
+       font = 2, cex=1.2)
+  invisible(bindat)
+}
+
---
+++ tests/test-07-member.R
@@ -0,0 +1,23 @@
+library(Polychrome)
+
+try( memberPlot( rep(0:1, 20)))                       # must be a matrix
+try( memberPlot(matrix(LETTERS, ncol = 2 )) )         # must be numeric
+try( memberPlot(matrix(1:30, ncol = 5), pal = palette()) ) # and binary
+
+temp <- t(data.frame(
+  A = c(rep(1, 500), rep(0, 100)),
+  B = c(rep(1, 500), rep(0, 100)),
+  C = c(rep(1, 300), rep(0, 300)),
+  D = c(rep(1, 300), rep(0, 300)),
+  E = c(rep(1, 300), rep(0, 300)),
+  F = c(rep(1, 180), rep(0, 320), rep(1, 40), rep(0, 60)),
+  G = c(rep(1, 180), rep(0, 320), rep(1, 40), rep(0, 60)),
+  H = c(rep(1, 180), rep(0, 320), rep(1, 40), rep(0, 60))
+))
+
+set.seed(54321)
+noise <- matrix(rbinom(prod(dim(temp)), 1, 0.05),
+                ncol = ncol(temp))
+meme <- 1*(temp | noise)
+memberPlot(meme)
+
